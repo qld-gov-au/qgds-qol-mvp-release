32d7576a083d41cf2829571a2a1b1d753f3a6597
duttonw <duttonw@users.noreply.github.com>
2024-06-24 03:44:37 +0000

    [Build and Test] Publish
    from qld-gov-au/qgds-bootstrap5:fixInit/dist REV:624d0ca3b58c16cfd02f6c5ce05357bfe3ef5199
    
    Clean up initilization system of partials. handlebarsHelpers and handlebarsPartials are modules that need to be called handlebarsHelpers(HandleBars)

--- components/handlebars.init.js
+++ components/handlebars.init.js
@@ -1,6 +1,32 @@
 import handlebarsHelpers from "./handlebars.helpers.js";
+import handlebarsPartials from "./handlebars.partials.js";
 import Handlebars from "handlebars";
-export * from "./handlebars.helpers.js";
 
-export const init = () => handlebarsHelpers(Handlebars);
+/**
+ * init function to load all HandleBar partials and helpers
+ *
+ * @param {Handlebars} handlebars
+ */
+let isHandlebarsHelpersAndPartialsRegistered = false;
+
+export default function init(handlebars = Handlebars) {
+  if (typeof(handlebars) === 'undefined') {
+    if(typeof(Handlebars) !== 'undefined') {
+      handlebars = Handlebars
+    }
+  }
+
+  if(typeof(handlebars) !== 'undefined') {
+    //only load once
+    if (!isHandlebarsHelpersAndPartialsRegistered) {
+      handlebarsHelpers(handlebars);
+      handlebarsPartials(handlebars);
+      isHandlebarsHelpersAndPartialsRegistered = true;
+    } else {
+      console.log("HandleBars Helpers And Partials already loaded")
+    }
+  } else {
+    console.log("Handlebars not found, init failed");
+  }
+}
 
