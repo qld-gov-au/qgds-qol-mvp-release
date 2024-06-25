82c77bbf4ecd46b5bf2e74f2242d5f4d4b110d7b
duttonw <duttonw@users.noreply.github.com>
2024-06-25 02:42:36 +0000

    [Build and Test] Publish
    from qld-gov-au/qgds-bootstrap5:rollbackHelperChanges/dist REV:6212947ee38d12b6cf41c10606337a70bba8b79a
    
    rollback to auto load helpers

--- components/handlebars.init.js
+++ components/handlebars.init.js
@@ -1,6 +1,33 @@
+/* global Handlebars */
 import handlebarsHelpers from "./handlebars.helpers.js";
-import Handlebars from "handlebars";
-export * from "./handlebars.helpers.js";
+import handlebarsPartials from "./handlebars.partials.js";
 
-export const init = () => handlebarsHelpers(Handlebars);
+
+/**
+ * init function to load all HandleBar partials and helpers
+ *
+ * @param {Handlebars} handlebars
+ */
+let isHandlebarsHelpersAndPartialsRegistered = false;
+
+export default function init(handlebars = Handlebars) {
+  if (typeof(handlebars) === 'undefined') {
+    console.error("Handlebars.init requires HandleBars");
+  }
+
+  if(typeof(handlebars) !== 'undefined') {
+    if (!isHandlebarsHelpersAndPartialsRegistered) {
+      isHandlebarsHelpersAndPartialsRegistered = true;
+    } else {
+      console.log("HandleBars Helpers And Partials already loaded, loading again")
+    }
+    handlebarsHelpers(handlebars);
+
+    handlebarsPartials(handlebars);
+  } else {
+    console.log("Handlebars not found, init failed");
+  }
+}
+
+init();
 
