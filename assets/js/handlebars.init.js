afbf62235b0a52515b90b21038bfee961a1ee943
jortronQGov <jortronQGov@users.noreply.github.com>
2024-06-26 05:48:08 +0000

    [Build and Test] Publish
    from qld-gov-au/qgds-bootstrap5:paragraphMarginFix/dist REV:79bf59611e5b36aae7183bd4cbd9581dc161de4b
    
    Remove top margin from first instance of paragraph tags in a container

--- assets/js/handlebars.init.js
+++
@@ -1,6 +0,0 @@
-import handlebarsHelpers from "./handlebars.helpers.js";
-import Handlebars from "handlebars";
-export * from "./handlebars.helpers.js";
-
-export const init = () => handlebarsHelpers(Handlebars);
-
